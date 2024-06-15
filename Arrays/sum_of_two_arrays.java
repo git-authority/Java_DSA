f2b88f4d5fabfc42d6bb1ca18accff2688822f7f
Abhrasnata Ray <122338508+git-authority@users.noreply.github.com>
2024-06-15 15:04:47 +0530

    Outputs sum of two arrays ( allowed input : 0-9  )

---
+++ Arrays/sum_of_two_arrays.java
@@ -0,0 +1,65 @@
+import java.util.*;
+public class sum_of_two_arrays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Size of First Array: ");
+        int n1 = in.nextInt();
+        System.out.println("Enter Size of Second Array: ");
+        int n2 = in.nextInt();
+        int size = 0, i = 0;
+        if(n1>n2)
+        {
+            size = n1;
+        }
+        else
+        {
+            size = n2;
+        }
+        int[] arr1 = new int [size];
+        int[] arr2 = new int [size];
+        if(n1>n2)
+        {
+            System.out.println("Enter Elemnts in First Array: ");
+            for(i=0; i<size; i++)
+            {
+                arr1[i]=in.nextInt();
+            }
+            System.out.println("Enter Elements in Second Array: ");
+            for(i=0;i<(size-n2);i++)
+            {
+                arr2[i]=0;
+            }
+            for(i=(size-n2);i<size;i++)
+            {
+                arr2[i]=in.nextInt();
+            }
+        }
+        else
+        {
+            System.out.println("Enter Elements in First Array: ");
+            for(i=0;i<(size-n1);i++)
+            {
+                arr1[i]=0;
+            }
+            for(i=(size-n1);i<size;i++)
+            {
+                arr1[i]=in.nextInt();
+            }
+            System.out.println("Enter Elemnts in Second Array: ");
+            for(i=0; i<size; i++)
+            {
+                arr2[i]=in.nextInt();
+            }
+        }
+        int[] sum = new int[size];
+        for(i=0;i<size;i++)
+        {
+            sum[i] = arr1[i] + arr2[i];
+        }
+        System.out.println("Sum of Respective Elements: ");
+        for(i=0;i<size;i++)
+        {
+            System.out.println(sum[i]);
+        }
+    }
+}
