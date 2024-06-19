52a26191e449c5036b397f4b2f7d690946f7b100
Abhrasnata Ray <122338508+git-authority@users.noreply.github.com>
2024-06-19 20:43:22 +0530

    Created alphaTriangle.java

---
+++ A2Z Sheet Solution/Pattern/alphaTriangle.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void alphaTriangle(int n) {
+        // Write your code here
+        int i = 0,j = 0;
+        for(i=65+n-1;i>=65;i--)
+        {
+            for(j=65+n-1; j>=i ;j--)
+            {
+                System.out.print((char)j+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
