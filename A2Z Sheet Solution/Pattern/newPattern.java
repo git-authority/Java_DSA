7203575120c1fd974d0fbea0ca08676186f2d85a
Abhrasnata Ray <122338508+git-authority@users.noreply.github.com>
2024-06-19 19:06:10 +0530

    Created newPattern.java

---
+++ A2Z Sheet Solution/Pattern/newPattern.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+        int i = 0, j = 0, k = 0;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(i=n-1;i>=1;i--)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
