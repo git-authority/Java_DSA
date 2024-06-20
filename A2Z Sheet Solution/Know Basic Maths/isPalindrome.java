f6a68e4cfa2c2bbbf09324c8e62d4969fc676264
Abhrasnata Ray <122338508+git-authority@users.noreply.github.com>
2024-06-20 12:48:59 +0530

    Created isPalindrome.java

---
+++ A2Z Sheet Solution/Know Basic Maths/isPalindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int num = x, rev = 0, r = 0;
+        if(x<0)
+        {
+            return false;
+        }
+        else if(x==0)
+        {
+            return true;
+        }
+        else
+        {
+            while(x>0)
+            {
+                r=x%10;
+                rev = rev*10+r;
+                x/=10;
+            }
+            if(rev==num)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
