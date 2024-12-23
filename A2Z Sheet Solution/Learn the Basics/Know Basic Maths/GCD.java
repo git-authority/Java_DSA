9e4d35f73ac22e2792ecda2018d4b42d388ef11f
Abhrasnata Ray <abhrasnataray@gmail.com>
2024-12-24 01:06:26 +0530

    Added Files

--- A2Z Sheet Solution/Learn the Basics/Know Basic Maths/GCD.java
+++ A2Z Sheet Solution/Learn the Basics/Know Basic Maths/GCD.java
@@ -1,30 +1,36 @@
-class GCD {
-    static Long[] lcmAndGcd(Long A , Long B) {
+public class GCD {
+    static Long[] lcmAndGcd(Long A, Long B) {
         // code here
-        long i = 0, gcd = 1, n1 = A, n2 = B;
+        long gcd = 1, n1 = A, n2 = B;
 
-        while(A>0 && B>0)
-        {
-            if(A>B)
-            {
+        while (A > 0 && B > 0) {
+            if (A > B) {
                 A = A % B;
-            }
-            else
-            {
+            } else {
                 B = B % A;
             }
         }
 
-        if(B == 0)
-        {
+        if (B == 0) {
             gcd = A;
-        }
-        else if ( A == 0)
-        {
+        } else if (A == 0) {
             gcd = B;
         }
-        long lcm = n1*n2 / gcd;
+        long lcm = n1 * n2 / gcd;
 
-        return new Long[]{lcm, gcd};
+        return new Long[] { lcm, gcd };
+    }
+
+    public static void main(String[] args) {
+        // Test values
+        Long A = 12L;
+        Long B = 15L;
+
+        // Call the lcmAndGcd function
+        Long[] result = lcmAndGcd(A, B);
+
+        // Print the results
+        System.out.println("LCM: " + result[0]);
+        System.out.println("GCD: " + result[1]);
     }
 }
