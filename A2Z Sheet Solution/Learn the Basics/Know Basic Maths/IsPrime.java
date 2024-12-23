96d5475281abc3ee7a57a96e2a626d75dbf407d4
Abhrasnata Ray <abhrasnataray@gmail.com>
2024-12-23 21:20:50 +0530

    Minor Changes

---
+++ A2Z Sheet Solution/Learn the Basics/Know Basic Maths/IsPrime.java
@@ -0,0 +1,15 @@
+class IsPrime {
+    public int reverse(int x) {
+        int r=0;
+        long num=0;
+        while(x!=0)
+        {
+            r=x%10;
+            num=num*10+r;
+            x/=10;
+            if (num > Integer.MAX_VALUE || num < Integer.MIN_VALUE)
+                return 0;
+        }
+        return (int) num;
+    }
+}
---
+++ A2Z Sheet Solution/Learn the Basics/Know Basic Maths/SumOfDivisors.java
@@ -0,0 +1,25 @@
+
+class SumOfDivisors{
+  static long sumOfDivisors(int N)
+  {
+        // code here
+        int i = 0, j = 0;
+        long div = 0;
+        for(i=1;i<=N;i++)
+        {
+            for(j=1;j<=((int)Math.sqrt(i));j++)
+            {
+                if(i%j == 0)
+                {
+                    div+=j;
+                    if(j!=i/j)
+                    {
+                        div+=i/j;
+
+                    }
+                }
+            }
+        }
+        return div;
+    }
+}
