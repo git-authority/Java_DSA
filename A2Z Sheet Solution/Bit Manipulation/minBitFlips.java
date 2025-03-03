a298512b81c81d8f145831128a292160d6e60029
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-03-03 23:34:20 +0530

    Made Some Changes

--- A2Z Sheet Solution/Bit Manipulation/minBitFlips.java
+++ A2Z Sheet Solution/Bit Manipulation/minBitFlips.java
@@ -4,10 +4,9 @@ class minBitFlips {
     public int solution(int start, int goal) {
         int ans = start ^ goal;
         int c = 0;
-        for (int i = 0; i <= 31; i++) {
-            if ((ans & (1 << i)) == 1) {
-                c++;
-            }
+        while(ans>0){
+            ans = ans & (ans-1);
+            c++;
         }
         return c;
     }
