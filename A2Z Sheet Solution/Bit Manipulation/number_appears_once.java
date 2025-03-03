0478dd9f7053a1c55d44079d123d219c99286e8f
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-03-03 23:48:03 +0530

    Solved a Question

---
+++ A2Z Sheet Solution/Bit Manipulation/number_appears_once.java
@@ -0,0 +1,11 @@
+
+public class number_appears_once {
+
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
