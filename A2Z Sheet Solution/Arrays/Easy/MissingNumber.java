6625bef25b66e3c49df13b5bf9d50d0f4065aa29
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-06-09 23:09:15 +0530

    Solved a new problem

---
+++ A2Z Sheet Solution/Arrays/Easy/MissingNumber.java
@@ -0,0 +1,15 @@
+public class MissingNumber {
+    // Function to find missing number in array
+    public int missingNumber(int[] nums) {
+        int xor1 = 0, xor2 = 0;
+
+        // Calculate XOR of all array elements
+        for (int i = 0; i < nums.length; i++) {
+            xor1 = xor1 ^ (i + 1); // XOR up to [1...N]
+            xor2 = xor2 ^ nums[i]; // XOR of array elements
+        }
+
+        // XOR of xor1 and xor2 gives missing number
+        return (xor1 ^ xor2);
+    }
+}
