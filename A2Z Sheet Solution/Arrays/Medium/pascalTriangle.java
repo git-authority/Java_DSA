b1296c657ab11262a11b7b37bf7b6727eecc588f
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-06-29 18:30:58 +0530

    Solved a Medium Question

---
+++ A2Z Sheet Solution/Arrays/Medium/pascalTriangle.java
@@ -0,0 +1,34 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class pascalTriangle {
+
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> finalAns = new ArrayList<>();
+
+        for (int row = 1; row <= numRows; row++) {
+            finalAns.add(result(row));
+        }
+
+        return finalAns;
+    }
+
+    private List<Integer> result(int row) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        ans.add(1);
+
+        int res = 1;
+
+        for (int col = 1; col < row; col++) {
+            res *= (row - col);
+            res /= (col);
+            ans.add(res);
+        }
+
+        return ans;
+    }
+}
