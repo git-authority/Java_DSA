93a981163cbb49878d2f606a3e3920c628b84684
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-07-06 20:57:06 +0530

    Solved an Easy Problem

---
+++ A2Z Sheet Solution/Binary Search/UpperBound.java
@@ -0,0 +1,27 @@
+public class UpperBound {
+  public int upperBound(int[] nums, int x) {
+
+    int lb = 0, ub = nums.length - 1, mid = 0;
+
+    while (lb <= ub) {
+
+        mid = (lb + ub) >> 1;
+
+        if (nums[mid] > x) {
+
+            ub = mid - 1;
+        }
+
+        else{
+
+            lb = mid + 1;
+
+        }
+
+    }
+
+    return lb;
+
+  }
+}
+
