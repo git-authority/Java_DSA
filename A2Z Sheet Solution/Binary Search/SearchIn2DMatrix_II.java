3b40fa767916575a84633f0b67ebebcdea52a995
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-07-16 20:36:58 +0530

    Solved a Medium Problem

---
+++ A2Z Sheet Solution/Binary Search/SearchIn2DMatrix_II.java
@@ -0,0 +1,22 @@
+public class SearchIn2DMatrix_II {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int row = 0, col = m - 1;
+
+        while(row<n && col>=0){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col]<target){
+                row++;
+            }
+            else{
+                col--;
+            }
+        }
+
+        return false;
+    }
+}
