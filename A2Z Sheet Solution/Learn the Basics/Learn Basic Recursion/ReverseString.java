4650e68591be4a7d4abbef8fa76ffbe5189286a8
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-07-28 19:41:39 +0530

    Solved an Easy Problem on Recursion

---
+++ A2Z Sheet Solution/Learn the Basics/Learn Basic Recursion/ReverseString.java
@@ -0,0 +1,18 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        reverse(s, left, right);
+    }
+
+    private void reverse(char[] s, int left, int right){
+        if(left>=right)    return;
+
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        reverse(s, left+1, right -1);
+
+    }
+}
