5cbf8b6823676fd58a195aa5c772f610c6dfeb0a
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-08-05 11:17:17 +0530

    Solved an Easy Question

---
+++ A2Z Sheet Solution/LinkedList/Singly LinkedList/InsertHead.java
@@ -0,0 +1,31 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+
+class InsertHead {
+    public ListNode insertAtHead(ListNode head, int X) {
+        ListNode newNode = new ListNode(X);
+
+        newNode.next = head;
+
+        head = newNode;
+
+        return head;
+    }
+}
