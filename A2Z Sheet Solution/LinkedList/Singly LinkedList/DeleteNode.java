a660db014dad18d3c712cb358f28a0ced815f6c9
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-08-05 10:47:01 +0530

    Made Slight Changes

--- A2Z Sheet Solution/LinkedList/Singly LinkedList/DeleteNode.java
+++ A2Z Sheet Solution/LinkedList/Singly LinkedList/DeleteNode.java
@@ -1,12 +1,13 @@
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
 
 public class DeleteNode {
 
