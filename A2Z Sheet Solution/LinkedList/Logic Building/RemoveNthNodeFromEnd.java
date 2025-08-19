2aaffbd35c4c1ccb48bf96d662b6a9f58b8147c5
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-08-19 10:26:01 +0530

    Solved a Medium Problem

---
+++ A2Z Sheet Solution/LinkedList/Logic Building/RemoveNthNodeFromEnd.java
@@ -0,0 +1,46 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode fastp = head;
+        ListNode slowp = head;
+
+        // Set the fast pointer
+        for(int i = 0;i<n;i++){
+            fastp = fastp.next;
+        }
+
+        if(fastp == null){
+            return head.next;
+        }
+
+        while(fastp.next!=null){
+            fastp = fastp.next;
+            slowp=slowp.next;
+        }
+
+        ListNode delNode = slowp.next;
+        slowp.next = slowp.next.next;
+        return head;
+    }
+}
