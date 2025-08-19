84d868d159d9e6549f376fffa5867cac81bf94fc
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-08-19 21:10:30 +0530

    Solved an Easy Probelm

---
+++ A2Z Sheet Solution/LinkedList/Logic Building/MiddleOfLinkedList.java
@@ -0,0 +1,35 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+
+class MiddleOfLinkedList {
+    public ListNode middleOfLinkedList(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
