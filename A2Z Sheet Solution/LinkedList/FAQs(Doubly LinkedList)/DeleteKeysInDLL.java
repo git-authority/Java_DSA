abf59655df43bb30ac5f823935b1aa5edff31f25
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-12-08 11:32:48 +0530

    Solved a Hard Problem

---
+++ A2Z Sheet Solution/LinkedList/FAQs(Doubly LinkedList)/DeleteKeysInDLL.java
@@ -0,0 +1,75 @@
+/*Definition of doubly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode prev;
+
+    ListNode() {
+        val = 0;
+        next = null;
+        prev = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+        prev = null;
+    }
+
+    ListNode(int data1, ListNode next1, ListNode prev1) {
+        val = data1;
+        next = next1;
+        prev = prev1;
+    }
+}
+ */
+
+class DeleteKeysInDLL {
+    public ListNode deleteAllOccurrences(ListNode head, int target) {
+
+        if (head == null){
+            return null;
+        }
+
+        // Dummy exists only to fix the head-deletion case
+        ListNode dummy = new ListNode(-1);
+
+        dummy.next = head;
+        head.prev = dummy;
+
+        ListNode current = head;
+
+        while(current!= null) {
+
+            if (current.val == target){
+
+                // Connect next pointers
+                current.prev.next = current.next;
+
+                // Connect previous pointers
+                if(current.next!= null){
+                    current.next.prev = current.prev;
+                }
+
+                ListNode nextNode = current.next;
+                current.next = null;
+                current.prev = null;
+                current = nextNode;
+
+            }
+            else{
+                current = current.next;
+            }
+        }
+
+
+        head = dummy.next;
+
+        if(head!= null){
+            head.prev = null;
+        }
+
+        return head;
+    }
+}
+
