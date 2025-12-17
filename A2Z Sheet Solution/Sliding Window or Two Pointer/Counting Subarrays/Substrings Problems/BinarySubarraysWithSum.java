460cca330f34334c6b720f408cf463854ba0b07f
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-12-18 00:28:21 +0530

    Solved a Hard Problem

---
+++ A2Z Sheet Solution/Sliding Window or Two Pointer/Counting Subarrays/Substrings Problems/BinarySubarraysWithSum.java
@@ -0,0 +1,36 @@
+class BinarySubarraysWithSum {
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+
+        return numSubarraysWithSumLessEqualToGoal(nums, goal) - numSubarraysWithSumLessEqualToGoal(nums, goal - 1);
+    }
+
+
+    private int numSubarraysWithSumLessEqualToGoal(int[] nums, int goal) {
+
+
+        if (goal < 0) return 0;
+
+        int l = 0, r = 0;
+
+        int sum = 0;
+
+        int count = 0;
+
+        while (r < nums.length) {
+            sum += nums[r];
+
+            while (sum > goal) {
+                sum -= nums[l];
+
+                l++;
+            }
+
+            count += (r - l + 1);
+
+            r++;
+        }
+
+        return count;
+    }
+}
